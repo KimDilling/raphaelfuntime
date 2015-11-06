f8ec3a9a9eec96a63759a7f21fadaac165d297df
Blair Johnson <helloblairj@gmail.com>
2015-11-06 14:27:32 -0600

    added images

--- our_script.js
+++ our_script.js
@@ -2,39 +2,132 @@
 window.onload = function() {
     var paper = new Raphael(document.getElementById('canvas_container'), 1000, 1000);
     //var circle = paper.circle(100, 100, 80);
+    
     var podium = paper.rect(100, 10, 50, 50);
+    var imgp = paper.image('images/seanwb.jpg', 100, 10, 50, 50);
+
+
 
     var table1a = paper.rect(100, 100, 50, 50);
+    var img1a = paper.image('images/steve.png', 100, 100, 50, 50);
+
     var table1b = paper.rect(150, 100, 50, 50);
+    var img1b = paper.image('images/akash.jpg', 150, 100, 50, 50);
+
+
     var table1c = paper.rect(200, 100, 50, 50);
+    var img1c = paper.image('images/bob.jpg', 200, 100, 50, 50);
+
+
     var table1d = paper.rect(250, 100, 50, 50);
+    var img1d = paper.image('images/erich.jpg', 250, 100, 50, 50);
+
+
+
+
 
     var table2a = paper.rect(350, 100, 50, 50);
+    var img2a = paper.image('images/natalie.jpg', 350, 100, 50, 50);
+
+
     var table2b = paper.rect(400, 100, 50, 50);
+    var img2b = paper.image('images/lauren.jpg', 400, 100, 50, 50);
+
+
     var table2c = paper.rect(450, 100, 50, 50);
+    var img2c = paper.image('images/kyle.jpg', 450, 100, 50, 50);
+
+
+
     var table2d = paper.rect(500, 100, 50, 50);
+    var img2d = paper.image('images/justin.jpg', 500, 100, 50, 50);
+
+
+
+
 
     var table3a = paper.rect(100, 200, 50, 50);
+    var img3a = paper.image('images/charlie.jpg', 100, 200, 50, 50);
+
+
     var table3b = paper.rect(150, 200, 50, 50);
+    var img3b = paper.image('images/marco.jpg', 150, 200, 50, 50);
+
+
     var table3c = paper.rect(200, 200, 50, 50);
+    var img3c = paper.image('images/adam.jpg', 200, 200, 50, 50);
+
+
     var table3d = paper.rect(250, 200, 50, 50);
+    var img3d = paper.image('images/seanh.jpg', 250, 200, 50, 50);
+
+
+
+
 
     var table4a = paper.rect(350, 200, 50, 50);
+    var img4a = paper.image('images/connor.jpg', 350, 200, 50, 50);
+
+
     var table4b = paper.rect(400, 200, 50, 50);
+    var img4b = paper.image('images/luke.jpg', 400, 200, 50, 50);
+
+
     var table4c = paper.rect(450, 200, 50, 50);
+    var img4c = paper.image('images/comic.jpg', 450, 200, 50, 50);
+
+
     var table4d = paper.rect(500, 200, 50, 50);
+    var img4d = paper.image('images/jordan.jpg', 500, 200, 50, 50);
+
+
+
+
+
 
 	var table5a = paper.rect(100, 300, 50, 50);
+    var img5a = paper.image('images/nicholas.jpg', 100, 300, 50, 50);
+
+
 	var table5b = paper.rect(150, 300, 50, 50);
+    var img5b = paper.image('images/alejandro.jpg', 150, 300, 50, 50);
+
+
 	var table5c = paper.rect(200, 300, 50, 50);
+    var img5c = paper.image('images/cody.jpg', 200, 300, 50, 50);
+
+
 	var table5d = paper.rect(250, 300, 50, 50);
+    var img5d = paper.image('images/rohit.jpg', 250, 300, 50, 50);
+
+
+
+
 
 	var table6a = paper.rect(350, 300, 50, 50);
-	var table6b = paper.rect(400, 300, 50, 50);
-	var table6c = paper.rect(450, 300, 50, 50);
-	var table6d = paper.rect(500, 300, 50, 50);	
+    var img6a = paper.image('images/larissa.jpg', 350, 300, 50, 50);
 
-	var teachtable1 = paper.rect(100, 450, 100, 50);
-	var teachtable2 = paper.rect(450, 450, 100, 50);		
-    //var ellipse = paper.ellipse(200, 400, 100, 50);
- 
+
+	var table6b = paper.rect(400, 300, 50, 50);
+    var img6b = paper.image('images/kim.jpg', 400, 300, 50, 50);
+
+
+	var table6c = paper.rect(450, 300, 50, 50);
+    var img6c = paper.image('images/bugsbunny.png', 450, 300, 50, 50);
+
+
+	var table6d = paper.rect(500, 300, 50, 50);	
+    var img6d = paper.image('images/blair.jpg', 500, 300, 50, 50);
+
+
+
+
+
+	var teachtable1 = paper.rect(150, 450, 50, 50);
+    var imgtt1 = paper.image('images/matt.jpg', 150, 450, 50, 50);
+
+	var teachtable2 = paper.rect(450, 450, 50, 50);
+    var imgtt2 = paper.image('images/mikewb.jpg', 450, 450, 50, 50);
+		
+ }
+
